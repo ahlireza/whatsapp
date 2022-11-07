c980e9462e6a5b5e8e48c70c88e134aa08b2c3ba
Alex Mahone <ahlireza@gmail.com>
2022-11-07 13:53:43 +0330

    code cchanged...

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, flash
 
 app = Flask(__name__)
+app.secret_key = b'\xb3\xf0\x1dR\x08o\xc8\xff'
 
 @app.route("/")
 def index():
     name01 = "name 01"
     msg01  = "Message 01"
-
+    flash(name01)
+    flash(msg01)
     name02 = "name 02"
     msg02  = "Message 02"
+    flash(name02)
+    flash(msg02)
 
-    return render_template("index.html", name_1 = name01, msg_1 = msg01, name_2 = name02, msg_2 = msg02)
+    # return render_template("index.html", name_1 = name01, msg_1 = msg01, name_2 = name02, msg_2 = msg02)
+    return render_template("index.html")
 
 
 @app.errorhandler(404)
